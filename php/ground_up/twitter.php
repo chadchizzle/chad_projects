16dc22d8bf96c6d40c6e51f69bf7c096e9d511e7
Chad D. Mills <bigchadchizzle@gmail.com>
2018-12-02 04:20:59 -0600

    Completed Twitter.php (Ver 1.0)
    Added Display logic (from Twitter API) Tested Successfully

--- php/ground_up/twitter.php
+++ php/ground_up/twitter.php
@@ -48,8 +48,19 @@ if(array_key_exists("errors", $string)) {echo "<h3>Sorry, there was a problem.</
 
 foreach($string as $items)
     {
-        // Do Stuff
+        // Render results from Twitter to browser
+		
+		        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+				echo "Tweet: ". $items['text']."<br />";
+				echo "Tweeted by: ". $items['user']['name']."<br />";
+				echo "Screen name: ". $items['user']['screen_name']."<br />";
+				echo "Followers: ". $items['user']['followers_count']."<br />";
+				echo "Friends: ". $items['user']['friends_count']."<br />";
+				echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+		
     }
+	
+	echo "<br><h2>We did it :)</h2>"
 ?>
 	
 </body>
