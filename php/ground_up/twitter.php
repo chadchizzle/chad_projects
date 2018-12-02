027f48781653e1c4f186c6b70c7a5a614debcb63
Chad D. Mills <bigchadchizzle@gmail.com>
2018-12-02 04:07:14 -0600

    Updated Twitter.php
    Added initial business logic

--- php/ground_up/twitter.php
+++ php/ground_up/twitter.php
@@ -6,10 +6,41 @@
 <body>
 <?php
 
-// 12/2/2018 Author: Chad D. Mills
-// Tools: Adobe Brackets + Easy Dev PHP Server
+/** 12/2/2018 Author: Chad D. Mills **/
+/** Tools: Adobe Brackets + Easy Dev PHP Server ^^/
 
 echo "<h2>Chad's Twitter API Script </h2>";
+require_once('TwitterAPIExchange.php');
+ 
+/** Set access tokens/app configuration via https://dev.twitter.com/apps/ **/
+
+$settings = array(
+    'oauth_access_token' => "YOUR_OAUTH_ACCESS_TOKEN",
+    'oauth_access_token_secret' => "YOUR_OAUTH_ACCESS_TOKEN_SECRET",
+    'consumer_key' => "YOUR_CONSUMER_KEY",
+    'consumer_secret' => "YOUR_CONSUMER_SECRET"
+	
+	
+/** construct URL variable & string	**/
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+
+/** connect to Twitter API using creditentials **/
+ 
+$requestMethod = "GET";
+ 
+$getfield = '?screen_name=bigchadchizzle&count=20';
+ 
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest();
+
+			 
+/** Render Results to Client **/
+
+
+
 
 ?>
 	
