87e2ff9f9a2e2b4458861017cce9eb6ce40f97f2
Chad D. Mills <bigchadchizzle@gmail.com>
2018-12-02 03:51:53 -0600

    Added Main Script File

---
+++ php/ground_up/twitter.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Twitter API Test (using PHP)</title>
+</head>
+<body>
+<?php
+
+// 12/2/2018 Author: Chad D. Mills
+// Tools: Adobe Brackets + Easy Dev PHP Server
+
+echo "<h2>Chad's Twitter API Script </h2>";
+
+?>
+	
+</body>
+</html>
